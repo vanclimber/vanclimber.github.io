e83d463c35c0b09ff80fc5102df614d5edc1f483
fancan <fancan@meituan.com>
2023-07-23 16:59:57 +0800

    chore(init):404页面

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,30 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueJsx from '@vitejs/plugin-vue-jsx'
-
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import vueJsx from "@vitejs/plugin-vue-jsx";
+import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        notFound: resolve(__dirname, "404.html"),
+      }
+    },
+  },
+  plugins: [
+    vue(),
+    vueJsx(),
+  ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
-      'balm-ui-plus': 'balm-ui/dist/balm-ui-plus.esm.js',
-      'balm-ui-css': 'balm-ui/dist/balm-ui.css',
-      '@views': fileURLToPath(new URL('./src/views', import.meta.url))
-    }
-  }
-})
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+      "balm-ui-plus": "balm-ui/dist/balm-ui-plus.esm.js",
+      "balm-ui-css": "balm-ui/dist/balm-ui.css",
+      "@views": fileURLToPath(new URL("./src/views", import.meta.url)),
+    },
+  },
+});
